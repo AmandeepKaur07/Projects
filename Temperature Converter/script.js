eef73d007dd9964e0425791c1942be0458e2552e
Amandeep Kaur <amandeepk78btechcse2024@kccitm.edu.in>
2023-08-20 22:16:59 +0530

    .

--- Temperature Converter/script.js
+++ Temperature Converter/script.js
@@ -19,6 +19,7 @@ function convertTemprature() {
       result.value = val.value;
     }
     
+    
     if (inputValue === "fahrenhiet" && outputValue === "celsius") {
       result.value = Number((val.value - 32) * 5) / 9;
     } else if (inputValue === "fahrenhiet" && outputValue === "kelvin") {
