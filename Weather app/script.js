9d591c29a5278fcd58fb5558976ae9d6bad9c750
Amandeep Kaur <amandeepk78btechcse2024@kccitm.edu.in>
2023-09-05 15:29:01 +0530

    .

--- Weather app/script.js
+++ Weather app/script.js
@@ -5,7 +5,6 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
   
 const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
 async function getWeatherByLocation(city){
      
          const resp = await fetch(url(city), {
